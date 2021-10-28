7d9b31d465bc755c1a1314efb61073a83481bfd8
Nilson <nilsonrx@gmail.com>
2021-10-27 22:56:42 -0300

    Modulo 04 : Aula_03

--- Modulo_04/Aula_03/users/users.module.ts
+++ Modulo_04/Aula_03/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaService } from '../netflix-clone/server/src/prisma.service';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule {
+
+}
